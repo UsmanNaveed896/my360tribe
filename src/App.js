50357a52fcc4a0f9c516ccda7640248fa964740f
Sadia Saeed <sadia.software@gmail.com>
2024-03-04 10:21:34 -0800

    authentication screens

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import Footer from './components/footer/footer';
 import Header from './components/header/header';
 import Homepage from './pages/homepagesections/container'
 import MainContainer from '../src/Popups/MainContainer'
-
+import ForgotPassword from '../src/Popups/ForgotPassword'
+import SignUp from '../src/Popups/SignUp2'
+import OTP from '../src/Popups/OTP'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <>
       {/* <Header />
       <Homepage />
       <Footer /> */}
-      <MainContainer/>
+      <OTP/>
+      {/* <Router>
+      <Routes>
+        <Route exact path="/" component={MainContainer} />
+        <Route path="/signup" component={SignUp} />
+      </Routes>
+    </Router> */}
     </>
   )
 }
