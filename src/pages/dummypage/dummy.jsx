96abf8bd1d44c845141ae47a23e44fc9aecdc0e0
Usman Naveed <56630061+UsmanNaveed896@users.noreply.github.com>
2024-03-07 00:06:00 +0500

    chart set

--- src/pages/dummypage/dummy.jsx
+++ src/pages/dummypage/dummy.jsx
@@ -11,7 +11,7 @@ const Dummy = () => {
       </div>
       <div className='flex justify-center bg-[#15214c] '>
         <div className='max-w-[800px]'>
-          <h1 className='text-white text-5xl mt-16 text-center'>MentorShip </h1>
+          <h1 className='text-white text-5xl mt-16 text-center'>Lorem ipsum dolor</h1>
           <div className='bg-white p-5 mt-12  h-[800px] overflow-y-scroll '>
             <h1 className='text-black font-bold'>1. Lorem ipsum dolor</h1>
             <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
