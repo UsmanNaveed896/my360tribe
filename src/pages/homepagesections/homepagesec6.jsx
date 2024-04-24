b77bd1429780c8cf2ba38e8ff541becb26a69137
unaveed21 <unaveed@azlaantechnologies.com>
2024-04-24 10:03:04 -0700

    meet our team

--- src/pages/homepagesections/homepagesec6.jsx
+++ src/pages/homepagesections/homepagesec6.jsx
@@ -76,19 +76,19 @@ const Homepagesec6 = () => {
                         <div className='flex justify-center'>
                             <div>
                                 <h1 className='text-[#0a194e] text-center font-bold text-5xl'>Meet our team</h1>
-                                <p className='text-[#0a194e] md:text-[35px] text-[25px] mt-6'>Founders and Founding Board </p>
+                                {/* <p className='text-[#0a194e] md:text-[35px] text-[25px] mt-6'>Founders and Founding Board </p> */}
                             </div>
                         </div>
-                        <div className='flex md:justify-between justify-center flex-wrap md:gap-0 gap-8'>
+                        {/* <div className='flex md:justify-between justify-center flex-wrap md:gap-0 gap-8'>
                             {employees.map((item) => (
                                 <Usercard name={item.name} designation={item.designation} experience={item.experience} />
                             ))}
-                        </div>
-                        <div className='flex justify-center mt-12'>
+                        </div> */}
+                        {/* <div className='flex justify-center mt-12'>
                             <div>
                                 <p className='text-[#0a194e] text-[35px] mt-6'>Advisory Board </p>
                             </div>
-                        </div>
+                        </div> */}
                         <div className='flex justify-center flex-wrap md:gap-32 gap-8 md:mt-12 mt-6'>
                             
                             {advisors.map((item) => (
