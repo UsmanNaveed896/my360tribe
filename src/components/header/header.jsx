962c4ee60c45e8e882aa2b46ea7359201e1e103b
unaveed21 <unaveed@azlaantechnologies.com>
2024-07-25 04:36:21 -0700

    conflict again solved

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,20 +5,14 @@ import PicsModal from "../../Popups/picsModal";
 import Aboutus from "../../Popups/Aboutus";
 import { AuthContext } from "../../pages/Authentication/authecontext";
 import { toast } from "react-toastify";
-
 const Header = ({ handleClick, isModalOpen, setIsModalOpen }) => {
-  const navigate = useNavigate();
   const { auth } = useContext(AuthContext);
-
+  const navigate = useNavigate();
   const gradient =
     "linear-gradient(to bottom, #ffffff, #b9b9b9, #777777, #3b3b3b, #000000)";
   const [isNavOpen, setIsNavOpen] = useState(false);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [logout, setLogout] = useState(false);
-
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
+  const [isOpen, setIsOpen] = useState(false);
   const toggleLogout = () => {
     setLogout(!logout);
   };
@@ -28,9 +22,12 @@ const Header = ({ handleClick, isModalOpen, setIsModalOpen }) => {
    toast.success("Successfully Logout!");
    window.location.reload()
   };
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
   const handleOptionClick = (option) => {
-    if (option === "Concierge") {
+    if (option == "Conceirge") {
       setIsModalOpen((st) => ({
         ...st,
         mod1: false,
@@ -38,7 +35,7 @@ const Header = ({ handleClick, isModalOpen, setIsModalOpen }) => {
         mod3: false,
         mod4: false,
       }));
-    } else if (option === "Peer Advocate Member") {
+    } else if (option == "Peer Advocate Member") {
       setIsModalOpen((st) => ({
         ...st,
         mod1: false,
@@ -55,9 +52,8 @@ const Header = ({ handleClick, isModalOpen, setIsModalOpen }) => {
         mod4: true,
       }));
     }
-    setIsDropdownOpen(false);
+    setIsOpen(false);
   };
-
   const img4 = {
     heading: " Service Partners",
     text: "We are always looking to build new partnerships that can support transitioning warriors. Please click here if you would like to partner with My360Tribe or to learn more",
@@ -67,35 +63,30 @@ const Header = ({ handleClick, isModalOpen, setIsModalOpen }) => {
     heading: "Concierge",
     text: "Are you passionate about making a difference in the life of a transitioning Operator?  Are you an Operator who has already transitioned and wants to give back to your Community?  Apply here to be a Concierge.",
   };
-
   const img1 = {
     heading: "Operators",
     text: "We are dedicated to serving those U.S. Special Operations members that were assessed, selected, trained, qualified, and served honorably as Operators at any of the U.S. Special Operations Commands, specifically, the U.S. Army Special Operations Command, U.S. Air Force Special Operations Command, U.S. Marine Forces Special Operations Command, Naval Special Warfare Command, and Joint Special Operations Command",
   };
-
   const img3 = {
     heading: "Peer Ambassador",
     text: "Are you a SOF Operator who has been out of the military for more than a year and want to support a newly transitioning Operator?",
   };
-
   const handleCancel = () => {
     // Implement cancel logic here
     console.log("Cancelling...");
     setIsModalOpen(false);
   };
-if(auth){
-  const photo = localStorage.getItem("photo");
-  var securePhotoUrl = photo?.replace("http://", "https://");
-}
-  
-console.log(auth,"aut")
+  if(auth){
+    const photo = localStorage.getItem("photo");
+    var securePhotoUrl = photo?.replace("http://", "https://");
+  }
   return (
     <div className="relative z-[999]">
       <nav
-        className="md:flex hidden items-center justify-between text-white px-2 py-3 fixed w-full top-0 z-[999]"
+        className="md:flex hidden items-center justify-between  text-white px-2 py-3 fixed w-full top-0 z-[999]"
         style={{ background: gradient }}
       >
-        <div className="w-[200px] relative">
+        <div className="w-[200px] relative ">
           <img
             className="cursor-pointer h-[70px] py-1"
             src={Img}
@@ -103,10 +94,11 @@ console.log(auth,"aut")
             onClick={() => navigate("/")}
           />
         </div>
-        <div className="flex items-center">
+        <div className="flex items-center ">
           <ul className="flex gap-8 font-semibold">
+            {/* <li className="mr-4"><i className='fa fa-user text-[20px]'></i></li> */}
             <li className="mr-4">
-              {auth ? (
+            {auth ? (
                 <div className="relative">
                   <div
                     className="flex gap-2 items-center cursor-pointer"
@@ -150,13 +142,14 @@ console.log(auth,"aut")
             >
               <a href="#">About Us</a>
             </li>
+            {/* <li className="mr-4"><a href="#">Get Involved</a></li> */}
             <li className="mr-4">
               <div className="relative">
                 <button
                   onClick={toggleDropdown}
-                  className="inline-flex justify-center items-center px-4 border border-transparent text-base font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2"
+                  className="inline-flex justify-center items-center px-4  border border-transparent text-base font-medium rounded-md text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 "
                 >
-                  Get Involved
+                  {"Get Involved"}
                   <svg
                     className="-mr-1 ml-2 h-5 w-5"
                     xmlns="http://www.w3.org/2000/svg"
@@ -172,7 +165,7 @@ console.log(auth,"aut")
                   </svg>
                 </button>
 
-                {isDropdownOpen && (
+                {isOpen && (
                   <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
                     <div
                       className="py-1"
@@ -184,7 +177,7 @@ console.log(auth,"aut")
                         href="#"
                         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
                         role="menuitem"
-                        onClick={() => handleOptionClick("Concierge")}
+                        onClick={() => handleOptionClick("Conceirge")}
                       >
                         Concierge
                       </a>
@@ -224,7 +217,7 @@ console.log(auth,"aut")
           </button>
         </div>
       </nav>
-      {/* Mobile Menu */}
+      {/* Mobile MEnu */}
       <div className="overflow-x-hidden block md:hidden">
         <nav className="" style={{ background: gradient }}>
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -232,95 +225,146 @@ console.log(auth,"aut")
               <img className="" src={Img} alt="logo" />
             </div>
             <div>
-              <div className="flex items-center justify-between md:hidden border-gray-400 py-8">
+              <div className="flex items-center justify-between  md:hidden border-gray-400 py-8">
                 <section className="MOBILE-MENU flex lg:hidden">
                   <div
-                    className="HAMBURGER-ICON space-y-2"
+                    className="HAMBURGER-ICON space-y-2 cursor-pointer"
                     onClick={() => setIsNavOpen((prev) => !prev)}
                   >
-                    <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
-                    <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
-                    <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
+                    <span className="block h-0.5 w-8 animate-pulse  bg-[black]"></span>
+                    <span className="block h-0.5 w-8 animate-pulse  bg-[black]"></span>
+                    <span className="block h-0.5 w-8 animate-pulse  bg-[black]"></span>
                   </div>
-
-                  <div
-                    className={`${
-                      isNavOpen ? "showMenuNav" : "hideMenuNav"
-                    } absolute top-0 left-0 w-full h-screen bg-white z-50`}
-                  >
+                  <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
                     <div
                       className="absolute top-0 right-0 px-8 py-8"
                       onClick={() => setIsNavOpen(false)}
                     >
                       <svg
-                        className="h-8 w-8 text-gray-800"
+                        className="h-8 w-8 text-gray-600"
                         viewBox="0 0 24 24"
                         fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       >
-                        <path
-                          d="M6 18L18 6M6 6l12 12"
-                          stroke="currentColor"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
+                        <line x1="18" y1="6" x2="6" y2="18" />
+                        <line x1="6" y1="6" x2="18" y2="18" />
                       </svg>
                     </div>
-                    <ul className="flex flex-col items-center justify-between min-h-[250px] gap-[40px] mt-40">
-                      <li className="text-2xl text-blue-700 font-semibold">
-                        <Link to="/">Home</Link>
+                    <ul className="flex flex-col items-center justify-between min-h-[250px]">
+                      <li className=" my-8 uppercase">
+                      {auth ? (
+                <div className="relative">
+                  <div
+                    className="flex gap-2 items-center cursor-pointer"
+                    onClick={toggleLogout}
+                  >
+                    <img
+                      className="rounded-full h-8 w-8"
+                      src={securePhotoUrl}
+                    />
+                    <p>{localStorage.getItem("name")}</p>
+                  </div>
+                  {logout && (
+                    <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg">
+                      <a
+                        href="#"
+                        className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+                        onClick={handleLogout}
+                      >
+                        Logout
+                      </a>
+                    </div>
+                  )}
+                </div>
+              ) : (
+                <Link to="/signin">
+                 <span className="text-white">Sign in</span>
+                </Link>
+              )}
                       </li>
-                      <li className="text-2xl text-blue-700 font-semibold">
+                      <li className="border-b border-gray-400 my-8 uppercase">
                         <a
-                          onClick={() =>
-                            setIsModalOpen((st) => ({
-                              ...st,
-                              mod5: true,
-                            }))
-                          }
+                          className="block py-2 pl-3 pr-4  text-white cursor-pointer font-semibold hover:font-bold"
+                          onClick={() => navigate("/")}
                         >
+                          Home
+                        </a>
+                      </li>
+                      <li
+                        className="border-b border-gray-400 my-8 uppercase cursor-pointer"
+                        onClick={() =>
+                          setIsModalOpen((st) => ({
+                            ...st,
+                            mod1: false,
+                            mod2: false,
+                            mod3: false,
+                            mod4: false,
+                            mod5: true,
+                          }))
+                        }
+                      >
+                        <a className="block py-2 pl-3 pr-4  text-white cursor-pointer font-semibold hover:font-bold">
                           About Us
                         </a>
                       </li>
-                      <li className="text-2xl text-blue-700 font-semibold">
-                        {auth ? (
-                          <div
-                            className="flex gap-2 items-center cursor-pointer"
-                            onClick={toggleDropdown}
-                          >
-                            <img
-                              className="rounded-full h-8 w-8"
-                              src={securePhotoUrl}
-                            />
-                            <p>{localStorage.getItem("name")}</p>
-                          </div>
-                        ) : (
-                          <Link to="/signin">
-                            <i className="fa fa-user text-[20px] mr-3"></i>Sign
-                            in
-                          </Link>
-                        )}
-                        {isDropdownOpen && (
-                          <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg">
-                            <a
-                              href="#"
-                              className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
-                              onClick={handleLogout}
-                            >
-                              Logout
-                            </a>
-                          </div>
-                        )}
+                      <li className="border-b border-gray-400 my-8 uppercase">
+                        <a className="block py-2 pl-3 pr-4  text-white cursor-pointer font-semibold hover:font-bold">
+                          Get Involved
+                        </a>
                       </li>
                     </ul>
                   </div>
                 </section>
+
+                <style>{`
+      .hideMenuNav {
+        display: none;
+      }
+      .showMenuNav {
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        background: ${gradient};
+        z-index: 10;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+      }
+    `}</style>
               </div>
             </div>
           </div>
         </nav>
       </div>
+
+      {isModalOpen.mod5 && (
+        <Aboutus
+          isOpen={isModalOpen}
+          onCancel={handleCancel}
+          setIsModalOpen={setIsModalOpen}
+        />
+      )}
+
+      <PicsModal
+        isOpen={isModalOpen}
+        onCancel={handleCancel}
+        data={
+          isModalOpen.mod1
+            ? img1
+            : isModalOpen.mod2
+            ? img2
+            : isModalOpen.mod3
+            ? img3
+            : img4
+        }
+      />
     </div>
   );
 };
